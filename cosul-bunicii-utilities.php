0311abf6292aeec203664dc17166510e8d508e61
simoaica <simoaica@yahoo.com>
2021-09-30 15:44:38 +0300

    Initial plugin settings

---
+++ cosul-bunicii-utilities.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Cosul Bunicii Utilities
+ *
+ * @package       CBUTILS
+ * @author        Dan Simoaica
+ * @version       1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:   Cosul Bunicii Utilities
+ * Plugin URI:    https://simoaica.ro
+ * Description:   Utilities stuff for cosulbunicii.ro
+ * Version:       1.0.0
+ * Author:        Dan Simoaica
+ * Author URI:    https://simoaica.ro
+ * Text Domain:   cosul-bunicii-utilities
+ * Domain Path:   /languages
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
